25919e760b3c1cb08a4f49bc9c4e76ad50d3c313
Vanesa <57409926+vanesa33@users.noreply.github.com>
2021-03-25 18:07:37 -0300

    tuplas prog funcional
    como utilizar las tuplas para poder guardar en variables funciones que tienen otras funciones.

---
+++ Tuplas y Prog Funcional/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Tuplas_y_Prog_Funcional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var van = Sol();
+
+            van.Counter.Increment();
+            Console.WriteLine(van.Counter.get());
+            van.SolDB.Add(1);
+
+        }
+
+        public static (
+
+            (Action Increment, Action Substract, Func<int> get) Counter,
+            (Action<int> Add, Action<int> Update, Action<int> Delete) SolDB
+            )
+
+            Sol() => (
+
+                Counter(),
+
+                SolDB()
+
+                );
+
+        private static (Action Increment, Action Substract, Func<int> get) Counter()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static (Action<int> Add, Action<int> Update, Action<int> Delete) SolDB()
+        {
+            throw new NotImplementedException();
+        }
+
+       
+    }
+}
